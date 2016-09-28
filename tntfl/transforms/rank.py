3118e9c08d643f930d86b148c76fcecacd4ed11d
Unknown <thomas_redston@outlook.com>
2016-09-28 20:01:10 +0100

    Remove unused variable

--- tntfl/transforms/rank.py
+++ tntfl/transforms/rank.py
@@ -15,20 +15,19 @@ def getPlayer(players, name):
     return players[name]
 
 
-def getActivePlayers(players, playedGames, time):
+def getActivePlayers(players, time):
     return [p for p in players.values() if (p.activeTillTime - time) > 0]
 
 
 def do(games):
     players = {}
-    playedGames = []
     secondsInactive = 60 * 60 * 24 * DAYS_INACTIVE
     for game in games:
         if not game.isDeleted():
             red = getPlayer(players, game.redPlayer)
             blue = getPlayer(players, game.bluePlayer)
 
-            activePlayers = getActivePlayers(players, playedGames, game.time - 1)
+            activePlayers = getActivePlayers(players, game.time - 1)
             sortedPlayers = sorted(activePlayers, key=lambda x: x.elo, reverse=True)
             redPosBefore = sortedPlayers.index(red) if red in sortedPlayers else -1
             bluePosBefore = sortedPlayers.index(blue) if blue in sortedPlayers else -1
@@ -53,6 +52,4 @@ def do(games):
             # It's this way around because a rise in position is to a lower numbered rank.
             game.bluePosChange = bluePosBefore - bluePosAfter if bluePosBefore >= 0 else 0
             game.redPosChange = redPosBefore - redPosAfter if redPosBefore >= 0 else 0
-
-            playedGames.append(game)
     return games
