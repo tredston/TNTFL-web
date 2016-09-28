d56828e87dc05d842f1b1cdc63b4d49d4fb9f2d1
Unknown <thomas_redston@outlook.com>
2016-09-28 20:08:00 +0100

    Only need to check previously active players
    Instead of all players

--- tntfl/transforms/rank.py
+++ tntfl/transforms/rank.py
@@ -15,22 +15,24 @@ def getPlayer(players, name):
     return players[name]
 
 
-def getActivePlayers(players, time):
-    return [p for p in players.values() if (p.activeTillTime - time) > 0]
+def filterActivePlayers(players, time):
+    return [p for p in players if (p.activeTillTime - time) > 0]
 
 
 def do(games):
     players = {}
+    activePlayers = []
     secondsInactive = 60 * 60 * 24 * DAYS_INACTIVE
     for game in games:
         if not game.isDeleted():
             red = getPlayer(players, game.redPlayer)
             blue = getPlayer(players, game.bluePlayer)
 
-            activePlayers = getActivePlayers(players, game.time - 1)
-            sortedPlayers = sorted(activePlayers, key=lambda x: x.elo, reverse=True)
-            redPosBefore = sortedPlayers.index(red) if red in sortedPlayers else -1
-            bluePosBefore = sortedPlayers.index(blue) if blue in sortedPlayers else -1
+            activePlayers = filterActivePlayers(activePlayers, game.time - 1)
+            activePlayers = sorted(activePlayers, key=lambda x: x.elo, reverse=True)
+
+            redPosBefore = activePlayers.index(red) if red in activePlayers else -1
+            bluePosBefore = activePlayers.index(blue) if blue in activePlayers else -1
 
             red.elo -= game.skillChangeToBlue
             blue.elo += game.skillChangeToBlue
@@ -38,13 +40,14 @@ def do(games):
             red.activeTillTime = activeTillTime
             blue.activeTillTime = activeTillTime
 
-            if red not in sortedPlayers:
-                sortedPlayers.append(red)
-            if blue not in sortedPlayers:
-                sortedPlayers.append(blue)
-            sortedPlayers = sorted(sortedPlayers, key=lambda x: x.elo, reverse=True)
-            redPosAfter = sortedPlayers.index(red)
-            bluePosAfter = sortedPlayers.index(blue)
+            if red not in activePlayers:
+                activePlayers.append(red)
+            if blue not in activePlayers:
+                activePlayers.append(blue)
+            activePlayers = sorted(activePlayers, key=lambda x: x.elo, reverse=True)
+
+            redPosAfter = activePlayers.index(red)
+            bluePosAfter = activePlayers.index(blue)
 
             game.bluePosAfter = bluePosAfter + 1  # 0-indexed -> 1-indexed
             game.redPosAfter = redPosAfter + 1
