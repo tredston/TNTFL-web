4fce6c2d3191b025ff024101e1ddd781423cafa0
Thomas Redston <thomas_redston@outlook.com>
2016-09-29 22:37:00 +0100

    Share tests

---
+++ tntfl/test/bases.py
@@ -0,0 +1,209 @@
+import abc
+import unittest
+
+
+class TestRunner(unittest.TestCase):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def _getJsonFrom(self, page, query=None):
+        pass
+
+    @abc.abstractmethod
+    def _testPageReachable(self, page, query=None):
+        """
+        Get the page and pass to _testResponse
+        """
+        pass
+
+    def _testResponse(self, response):
+        self.assertTrue("Traceback (most recent call last):" not in response)
+
+
+class Pages(object):
+    def testIndexReachable(self):
+        self._testPageReachable('index.cgi')
+
+    def testAchievementsReachable(self):
+        self._testPageReachable('achievements.cgi')
+
+    def testApiReachable(self):
+        self._testPageReachable('api.cgi')
+
+    def testGameReachable(self):
+        self._testPageReachable('game.cgi', 'method=view&game=1223308996')
+
+    def testPlayerReachable(self):
+        self._testPageReachable('player.cgi', 'player=jrem')
+
+    def testPlayerGamesReachable(self):
+        self._testPageReachable('player.cgi', 'player=jrem&method=games')
+
+    def testHeadToHeadReachable(self):
+        self._testPageReachable('headtohead.cgi', 'player1=jrem&player2=sam')
+
+    def testHeadToHeadGamesReachable(self):
+        self._testPageReachable('headtohead.cgi', 'player1=jrem&player2=sam&method=games')
+
+    def testSpeculateReachable(self):
+        self._testPageReachable('speculate.cgi')
+
+    def testStatsReachable(self):
+        self._testPageReachable('stats.cgi')
+
+    def testHistoricReachable(self):
+        self._testPageReachable('historic.cgi')
+
+    def _testResponse(self, response):
+        super(Pages, self)._testResponse(response)
+        self.assertTrue("<!DOCTYPE html>" in response)
+
+
+class SpeculatePage(object):
+    def testAGame(self):
+        self._testPageReachable('speculate.cgi', 'redPlayer=tlr&redScore=10&blueScore=0&bluePlayer=cjm&previousGames=')
+
+    def testMultipleGames(self):
+        self._testPageReachable('speculate.cgi', 'redPlayer=acas&redScore=10&blueScore=0&bluePlayer=epb&previousGames=tlr%2C10%2C0%2Ccjm%2Cjma%2C10%2C0%2Cmsh')
+
+    def _testResponse(self, response):
+        super(SpeculatePage, self)._testResponse(response)
+        self.assertTrue("<!DOCTYPE html>" in response)
+        self.assertTrue('Speculative Ladder' in response)
+
+
+class PageBits(object):
+    def testLadderReachable(self):
+        self._testPageReachable('ladder.cgi')
+
+    def testRecentReachable(self):
+        self._testPageReachable('recent.cgi')
+
+
+class PlayerApi(object):
+    def testPlayerJson(self):
+        response = self._getJsonFrom('player.cgi', 'player=ndt&view=json')
+        self.assertEqual(response['name'], "ndt")
+        self.assertEqual(response['rank'], -1)
+        self.assertEqual(response['active'], False)
+        self.assertEqual(response['skill'], 65.7308777725)
+        self.assertEqual(response['overrated'], -20.2998078551)
+        self.assertEqual(response['total']['for'], 2895)
+        self.assertEqual(response['total']['against'], 2005)
+        self.assertEqual(response['total']['games'], 490)
+        self.assertEqual(response['total']['wins'], 286)
+        self.assertEqual(response['total']['losses'], 96)
+        self.assertEqual(response['total']['gamesToday'], 0)
+
+    def testPlayerGamesJsonReachable(self):
+        response = self._getJsonFrom('player.cgi', 'player=ndt&method=games&view=json')
+        self.assertEqual(len(response), 490)
+        self.assertEqual(response[0]['date'], 1392725064)
+
+
+class HeadToHeadApi(object):
+    def testHeadToHeadGamesJsonReachable(self):
+        response = self._getJsonFrom('headtohead.cgi', 'player1=cjm&player2=ndt&method=games&view=json')
+        self.assertEqual(len(response), 9)
+        self.assertEqual(response[0]['date'], 1394037228)
+
+
+class RecentApi(object):
+    def testRecentJsonReachable(self):
+        response = self._getJsonFrom('recent.cgi', 'view=json')
+
+
+class LadderApi(object):
+    def testReachable(self):
+        response = self._getJsonFrom('ladder.cgi', 'view=json')
+
+    def testRange(self):
+        response = self._getJsonFrom('ladder.cgi', 'gamesFrom=1223308996&gamesTo=1223400000&view=json')
+        self.assertEqual(len(response), 3)
+        self.assertEqual(response[0]['rank'], 1)
+        self.assertEqual(response[0]['name'], 'jrem')
+        self.assertEqual(response[0]['skill'], 16.5027380839)
+        self.assertEqual(response[2]['rank'], 3)
+        self.assertEqual(response[2]['name'], 'kjb')
+        self.assertEqual(response[2]['skill'], -12.5)
+
+
+class GameApi(object):
+    def test(self):
+        response = self._getJsonFrom('game.cgi', 'method=view&game=1223308996&view=json')
+        self.assertEqual(response['red']['name'], 'jrem')
+        self.assertEqual(response['red']['href'], '../../player/jrem/json')
+        self.assertEqual(response['red']['score'], 10)
+        self.assertEqual(response['red']['skillChange'], 14.8698309141)
+        self.assertEqual(response['red']['rankChange'], 0)
+        self.assertEqual(response['red']['newRank'], 15)
+        redAchievements = response['red']['achievements']
+        self.assertEqual(len(redAchievements), 3)
+        self.assertEqual(redAchievements[0]['name'], "Flawless Victory")
+        self.assertEqual(redAchievements[0]['description'], "Beat an opponent 10-0")
+        self.assertEqual(redAchievements[1]['name'], "Early Bird")
+        self.assertEqual(redAchievements[1]['description'], "Play and win the first game of the day")
+        self.assertEqual(redAchievements[2]['name'], "Pok&#233;Master")
+        self.assertEqual(redAchievements[2]['description'], "Collect all the scores")
+
+        self.assertEqual(response['blue']['name'], 'kjb')
+        self.assertEqual(response['blue']['href'], '../../player/kjb/json')
+        self.assertEqual(response['blue']['score'], 0)
+        self.assertEqual(response['blue']['skillChange'], -14.8698309141)
+        self.assertEqual(response['blue']['rankChange'], 0)
+        self.assertEqual(response['blue']['newRank'], 14)
+        self.assertEqual(response['blue']['achievements'], [])
+
+        self.assertEqual(response['positionSwap'], False)
+        self.assertEqual(response['date'], 1223308996)
+
+    def testPositionSwap(self):
+        response = self._getJsonFrom('game.cgi', 'method=view&game=1443785561&view=json')
+        self.assertEqual(response['positionSwap'], True)
+        self.assertEqual(response['date'], 1443785561)
+
+
+class GamesApi(object):
+    def test(self):
+        response = self._getJsonFrom('games.cgi', 'view=json&from=1120830176&to=1120840777')
+        self.assertEqual(len(response), 3)
+
+        self.assertEqual(response[0]['red']['name'], 'lefh')
+        self.assertEqual(response[0]['red']['href'], 'player/lefh/json')
+        self.assertEqual(response[0]['red']['score'], 5)
+        self.assertEqual(response[0]['red']['skillChange'], -0.497657033239)
+        self.assertEqual(response[0]['red']['rankChange'], 0)
+        self.assertEqual(response[0]['red']['newRank'], 2)
+        self.assertEqual(response[0]['red']['achievements'], [])
+
+        self.assertEqual(response[0]['blue']['name'], 'pdw')
+        self.assertEqual(response[0]['blue']['href'], 'player/pdw/json')
+        self.assertEqual(response[0]['blue']['score'], 5)
+        self.assertEqual(response[0]['blue']['skillChange'], 0.497657033239)
+        self.assertEqual(response[0]['blue']['rankChange'], 0)
+        self.assertEqual(response[0]['blue']['newRank'], 3)
+        self.assertEqual(response[0]['blue']['achievements'], [])
+
+        self.assertEqual(response[0]['positionSwap'], False)
+        self.assertEqual(response[0]['date'], 1120830176)
+        self.assertEqual(response[1]['date'], 1120834874)
+        self.assertEqual(response[2]['date'], 1120840777)
+
+    def testLimit(self):
+        response = self._getJsonFrom('games.cgi', 'view=json&from=1448887743&to=1448897743&limit=2')
+        self.assertEqual(len(response), 2)
+        self.assertEqual(response[0]['date'], 1448895666)
+        self.assertEqual(response[1]['date'], 1448897511)
+
+    def testDeleted(self):
+        response = self._getJsonFrom('games.cgi', 'view=json&from=1448887743&to=1448890745&includeDeleted=1')
+        self.assertEqual(len(response), 4)
+        self.assertEqual(response[0]['deleted']['at'], 1448889773)
+        self.assertEqual(response[0]['deleted']['by'], 'tlr')
+        self.assertEqual(response[0]['date'], 1448889571)
+        self.assertEqual(response[1]['date'], 1448889749)
+
+    def testNoDeleted(self):
+        response = self._getJsonFrom('games.cgi', 'view=json&from=1448887743&to=1448890745&includeDeleted=0')
+        self.assertEqual(len(response), 3)
+        self.assertEqual(response[0]['date'], 1448889749)
--- tntfl/test/test_deployment.py
+++ tntfl/test/test_deployment.py
@@ -3,32 +3,28 @@ import unittest
 import urlparse
 import json
 import os
+import tntfl.test.bases as Bases
 
 
 class Deployment(unittest.TestCase):
     urlBase = os.path.join('http://www/~tlr/', os.path.split(os.getcwd())[1]) + "/"
 
-    def _page(self, page):
-        return urlparse.urljoin(self.urlBase, page)
+    def _page(self, page, query=None):
+        return urlparse.urljoin(self.urlBase, page) + ('?' + query) if (query is not None) else ''
 
-    def _getJsonFrom(self, page):
-        response = urllib2.urlopen(self._page(page))
+    def _getJsonFrom(self, page, query=None):
+        response = urllib2.urlopen(self._page(page, query))
+        self.assertTrue(len(response) > 0)
         return json.load(response)
 
-    def _testPageReachable(self, page):
-        response = urllib2.urlopen(self._page(page))
+    def _testPageReachable(self, page, query=None):
+        response = urllib2.urlopen(self._page(page, query))
         self._testResponse(response.read())
 
-    def _testResponse(self, response):
-        self.assertTrue("Traceback (most recent call last):" not in response)
 
-
-class Pages(Deployment):
+class Redirects(Deployment):
     def testIndexReachable(self):
         self._testPageReachable('')
-
-    def testAchievementsReachable(self):
-        self._testPageReachable('achievements.cgi')
 
     def testApiReachable(self):
         self._testPageReachable('api/')
@@ -36,47 +32,48 @@ class Pages(Deployment):
     def testGameReachable(self):
         self._testPageReachable('game/1223308996/')
 
+    def testGameJsonReachable(self):
+        self._getJsonFrom('game/1223308996/json')
+
     def testHeadToHeadReachable(self):
         self._testPageReachable('headtohead/jrem/sam/')
 
     def testPlayerReachable(self):
         self._testPageReachable('player/jrem/')
 
+    def testPlayerJsonReachable(self):
+        self._getJsonFrom('player/ndt/json')
+
     def testPlayerGamesReachable(self):
         self._testPageReachable('player/jrem/games/')
+
+    def testPlayerGamesJsonReachable(self):
+        self._getJsonFrom('player/ndt/games/json')
 
     def testHeadToHeadGamesReachable(self):
         self._testPageReachable('headtohead/jrem/ndt/games/')
 
-    def testStatsReachable(self):
-        self._testPageReachable('stats/')
+    def testHeadToHeadGamesJsonReachable(self):
+        self._getJsonFrom('headtohead/cjm/ndt/games/json')
 
-    def testHistoricReachable(self):
-        self._testPageReachable('historic.cgi')
-
-    def _testResponse(self, response):
-        super(Pages, self)._testResponse(response)
-        self.assertTrue("<!DOCTYPE html>" in response)
-
-
-class SpeculatePage(Deployment):
     def testReachable(self):
         self._testPageReachable('speculate/')
 
-    def testAGame(self):
-        self._testSpeculatePageReachable('speculate.cgi?redPlayer=tlr&redScore=10&blueScore=0&bluePlayer=cjm&previousGames=')
+    def testStatsReachable(self):
+        self._testPageReachable('stats/')
 
-    def testMultipleGames(self):
-        self._testSpeculatePageReachable('speculate.cgi?redPlayer=acas&redScore=10&blueScore=0&bluePlayer=epb&previousGames=tlr%2C10%2C0%2Ccjm%2Cjma%2C10%2C0%2Cmsh')
+    def testRecentJsonReachable(self):
+        self._getJsonFrom('recent/json')
 
-    def _testSpeculatePageReachable(self, page):
-        response = urllib2.urlopen(self._page(page))
-        self._testSpeculateResponse(response.read())
+    def testLadderJsonReachable(self):
+        self._getJsonFrom('ladder/json')
 
-    def _testSpeculateResponse(self, response):
-        self._testResponse(response)
-        self.assertTrue("<!DOCTYPE html>" in response)
-        self.assertTrue('Speculative Ladder' in response)
+    def testLadderRangeJsonReachable(self):
+        self._getJsonFrom('ladder/?gamesFrom=1223308996&gamesTo=1223400000&view=json')
+
+    def _testResponse(self, response):
+        super(Pages, self)._testResponse(response)
+        self.assertFalse("<!DOCTYPE html>" in response)
 
 
 class DeletePage(Deployment):
@@ -103,151 +100,37 @@ class DeletePage(Deployment):
         self.assertTrue("<!DOCTYPE html>" in response)
 
 
-class PageBits(Deployment):
-    def testLadderReachable(self):
-        self._testPageReachable('ladder.cgi')
-
-    def testRecentReachable(self):
-        self._testPageReachable('recent.cgi')
+class Pages(Deployment, Bases.Pages):
+    pass
 
 
-class PlayerApi(Deployment):
-    def testPlayerJson(self):
-        page = 'player/ndt/json'
-        response = self._getJsonFrom(page)
-        self.assertEqual(response['name'], "ndt")
-        self.assertEqual(response['rank'], -1)
-        self.assertEqual(response['active'], False)
-        self.assertEqual(response['skill'], 65.7308777725)
-        self.assertEqual(response['overrated'], -20.2998078551)
-        self.assertEqual(urlparse.urljoin(self._page(page), response['games']['href']), urlparse.urljoin(self.urlBase, "player/ndt/games/json"))
-        self.assertEqual(response['total']['for'], 2895)
-        self.assertEqual(response['total']['against'], 2005)
-        self.assertEqual(response['total']['games'], 490)
-        self.assertEqual(response['total']['wins'], 286)
-        self.assertEqual(response['total']['losses'], 96)
-        self.assertEqual(response['total']['gamesToday'], 0)
-
-    def testPlayerGamesJsonReachable(self):
-        response = self._getJsonFrom('player/ndt/games/json')
-        self.assertEqual(len(response), 490)
-        self.assertEqual(response[0]['date'], 1392725064)
+class SpeculatePage(Deployment, Bases.SpeculatePage):
+    pass
 
 
-class HeadToHeadApi(Deployment):
-    def testHeadToHeadGamesJsonReachable(self):
-        response = self._getJsonFrom('headtohead/cjm/ndt/games/json')
-        self.assertEqual(len(response), 9)
-        self.assertEqual(response[0]['date'], 1394037228)
+class PageBits(Deployment, Bases.PageBits):
+    pass
 
 
-class RecentApi(Deployment):
-    def testRecentJsonReachable(self):
-        response = self._getJsonFrom('recent/json')
+class PlayerApi(Deployment, Bases.PlayerApi):
+    pass
 
 
-class LadderApi(Deployment):
-    def testReachable(self):
-        response = self._getJsonFrom('ladder/json')
-
-    def testRange(self):
-        page = 'ladder/?gamesFrom=1223308996&gamesTo=1223400000&view=json'
-        response = self._getJsonFrom(page)
-        self.assertEqual(len(response), 3)
-        self.assertEqual(response[0]['rank'], 1)
-        self.assertEqual(response[0]['name'], 'jrem')
-        self.assertEqual(response[0]['skill'], 16.5027380839)
-        self.assertEqual(urlparse.urljoin(self._page(page), response[0]['href']), urlparse.urljoin(self.urlBase, "player/jrem/json"))
-        self.assertEqual(response[2]['rank'], 3)
-        self.assertEqual(response[2]['name'], 'kjb')
-        self.assertEqual(response[2]['skill'], -12.5)
-        self.assertEqual(urlparse.urljoin(self._page(page), response[2]['href']), urlparse.urljoin(self.urlBase, "player/kjb/json"))
+class HeadToHeadApi(Deployment, Bases.HeadToHeadApi):
+    pass
 
 
-class GameApi(Deployment):
-    def test(self):
-        page = 'game/1223308996/json'
-        response = self._getJsonFrom(page)
-        self.assertEqual(response['red']['name'], 'jrem')
-        self.assertEqual(response['red']['href'], '../../player/jrem/json')
-        self.assertEqual(urlparse.urljoin(self._page(page), response['red']['href']), urlparse.urljoin(self.urlBase, "player/jrem/json"))
-        self.assertEqual(response['red']['score'], 10)
-        self.assertEqual(response['red']['skillChange'], 14.8698309141)
-        self.assertEqual(response['red']['rankChange'], 0)
-        self.assertEqual(response['red']['newRank'], 15)
-        redAchievements = response['red']['achievements']
-        self.assertEqual(len(redAchievements), 3)
-        self.assertEqual(redAchievements[0]['name'], "Flawless Victory")
-        self.assertEqual(redAchievements[0]['description'], "Beat an opponent 10-0")
-        self.assertEqual(redAchievements[1]['name'], "Early Bird")
-        self.assertEqual(redAchievements[1]['description'], "Play and win the first game of the day")
-        self.assertEqual(redAchievements[2]['name'], "Pok&#233;Master")
-        self.assertEqual(redAchievements[2]['description'], "Collect all the scores")
-
-        self.assertEqual(response['blue']['name'], 'kjb')
-        self.assertEqual(response['blue']['href'], '../../player/kjb/json')
-        self.assertEqual(response['blue']['score'], 0)
-        self.assertEqual(response['blue']['skillChange'], -14.8698309141)
-        self.assertEqual(response['blue']['rankChange'], 0)
-        self.assertEqual(response['blue']['newRank'], 14)
-        self.assertEqual(response['blue']['achievements'], [])
-
-        self.assertEqual(response['positionSwap'], False)
-        self.assertEqual(response['date'], 1223308996)
-
-    def testPositionSwap(self):
-        page = 'game/1443785561/json'
-        response = self._getJsonFrom(page)
-        self.assertEqual(response['positionSwap'], True)
-        self.assertEqual(response['date'], 1443785561)
+class RecentApi(Deployment, Bases.RecentApi):
+    pass
 
 
-class GamesApi(Deployment):
-    def test(self):
-        page = 'games.cgi?view=json&from=1120830176&to=1120840777'
-        response = self._getJsonFrom(page)
-        self.assertEqual(len(response), 3)
+class LadderApi(Deployment, Bases.LadderApi):
+    pass
 
-        self.assertEqual(response[0]['red']['name'], 'lefh')
-        self.assertEqual(response[0]['red']['href'], 'player/lefh/json')
-        self.assertEqual(urlparse.urljoin(self._page(page), response[0]['red']['href']), urlparse.urljoin(self.urlBase, "player/lefh/json"))
-        self.assertEqual(response[0]['red']['score'], 5)
-        self.assertEqual(response[0]['red']['skillChange'], -0.497657033239)
-        self.assertEqual(response[0]['red']['rankChange'], 0)
-        self.assertEqual(response[0]['red']['newRank'], 2)
-        self.assertEqual(response[0]['red']['achievements'], [])
 
-        self.assertEqual(response[0]['blue']['name'], 'pdw')
-        self.assertEqual(response[0]['blue']['href'], 'player/pdw/json')
-        self.assertEqual(response[0]['blue']['score'], 5)
-        self.assertEqual(response[0]['blue']['skillChange'], 0.497657033239)
-        self.assertEqual(response[0]['blue']['rankChange'], 0)
-        self.assertEqual(response[0]['blue']['newRank'], 3)
-        self.assertEqual(response[0]['blue']['achievements'], [])
+class GameApi(Deployment, Bases.GameApi):
+    pass
 
-        self.assertEqual(response[0]['positionSwap'], False)
-        self.assertEqual(response[0]['date'], 1120830176)
-        self.assertEqual(response[1]['date'], 1120834874)
-        self.assertEqual(response[2]['date'], 1120840777)
 
-    def testLimit(self):
-        page = 'games.cgi?view=json&from=1448887743&to=1448897743&limit=2'
-        response = self._getJsonFrom(page)
-        self.assertEqual(len(response), 2)
-        self.assertEqual(response[0]['date'], 1448895666)
-        self.assertEqual(response[1]['date'], 1448897511)
-
-    def testDeleted(self):
-        page = 'games.cgi?view=json&from=1448887743&to=1448890745&includeDeleted=1'
-        response = self._getJsonFrom(page)
-        self.assertEqual(len(response), 4)
-        self.assertEqual(response[0]['deleted']['at'], 1448889773)
-        self.assertEqual(response[0]['deleted']['by'], 'tlr')
-        self.assertEqual(response[0]['date'], 1448889571)
-        self.assertEqual(response[1]['date'], 1448889749)
-
-    def testNoDeleted(self):
-        page = 'games.cgi?view=json&from=1448887743&to=1448890745&includeDeleted=0'
-        response = self._getJsonFrom(page)
-        self.assertEqual(len(response), 3)
-        self.assertEqual(response[0]['date'], 1448889749)
+class GamesApi(Deployment, Bases.GamesApi):
+    pass
