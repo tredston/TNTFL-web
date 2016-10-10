224b3226e96ec977fb0fb2003433e60a3caf2f0b
Thomas Redston <thomas_redston@outlook.com>
2016-10-10 19:47:57 +0100

    Tidy

--- ui/components/game-details.tsx
+++ ui/components/game-details.tsx
@@ -5,14 +5,6 @@ import { Grid, Panel, Row, Col } from 'react-bootstrap';
 import Achievement from '../model/achievement';
 import Game from '../model/game';
 
-function JsonLink(): JSX.Element {
-  return (
-    <div className="jsonLink">
-      <p><a href="json">This game as JSON</a></p>
-    </div>
-  );
-}
-
 interface AchievementItemProps {
   achievement: Achievement;
 }
@@ -23,27 +15,11 @@ function AchievementItem(props: AchievementItemProps) {
     "achievement-" + achievement.name.replace(/ /g, '')
   );
   return (
-    <div className="achievement">
-      <Panel header={achievement.name}>
-        <div className={icon} style={{textAlign: 'center'}}>
-        {achievement.description}
-        </div>
-      </Panel>
-    </div>
-  );
-}
-
-interface AchievementListProps {
-  achievements: Achievement[];
-}
-function AchievementList(props: AchievementListProps): JSX.Element {
-  const { achievements } = props;
-  return (
-    <div className="achievements">
-      <Col md={4}>
-        {achievements.map((ach, i) => <AchievementItem achievement={ach} key={`achi${i}`}/>)}
-      </Col>
-    </div>
+    <Panel header={achievement.name}>
+      <div className={icon} style={{textAlign: 'center'}}>
+      {achievement.description}
+      </div>
+    </Panel>
   );
 }
 
@@ -66,11 +42,9 @@ interface PunditryProps {
 function Punditry(props: PunditryProps): JSX.Element {
   const { facts } = props;
   return (
-    <div className="punditry">
-      <Panel title={'Punditry'}>
-        {facts.map((fact) => <Fact fact={fact}/>)}
-      </Panel>
-    </div>
+    <Panel title={'Punditry'}>
+      {facts.map((fact) => <Fact fact={fact}/>)}
+    </Panel>
   );
 }
 
@@ -82,15 +56,19 @@ export default function GameDetails(props: GameDetailsProps): JSX.Element {
   return (
     <Grid>
       <Row>
-        <AchievementList achievements={game.red.achievements}/>
+        <Col md={4}>
+          {game.red.achievements.map((ach, i) => <AchievementItem achievement={ach} key={`achr${i}`}/>)}
+        </Col>
         <Col md={4}>
           {game.punditry && game.punditry.length ? <Punditry facts={game.punditry} /> : null}
         </Col>
-        <AchievementList achievements={game.blue.achievements}/>
+        <Col md={4}>
+          {game.blue.achievements.map((ach, i) => <AchievementItem achievement={ach} key={`achr${i}`}/>)}
+        </Col>
       </Row>
       <Row>
-      <JsonLink/>
-      {!game.deleted ? <a href="delete" className="btn btn-danger pull-right"><span className="glyphicon glyphicon-lock"></span> Delete game</a> : null}
+        <p><a href="json">This game as JSON</a></p>
+        {!game.deleted ? <a href="delete" className="btn btn-danger pull-right"><span className="glyphicon glyphicon-lock"></span> Delete game</a> : null}
       </Row>
     </Grid>
   );
