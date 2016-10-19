286221875ea574bf84dcdca8afe9689d1bc99603
Thomas Redston <tlr@corefiling.com>
2016-10-19 16:54:10 +0100

    Use chartjs on player page

--- ui/player.tsx
+++ ui/player.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { Component, Props, CSSProperties } from 'react';
 import { Panel, Grid, Row, Col } from 'react-bootstrap';
 import * as ReactDOM from 'react-dom';
-import { VictoryChart, VictoryLine, VictoryAxis } from 'victory';
+import { Line } from 'react-chartjs-2';
 
 import GameList from './components/game-list';
 import NavigationBar from './components/navigation-bar';
 import Game from './model/game';
 import Player from './model/player';
-import { getParameterByName, getLadderLeagueClass } from './utils/utils';
+import { getParameterByName, getLadderLeagueClass, formatEpoch } from './utils/utils';
 
 interface StatBoxProps {
   title: string;
@@ -106,19 +106,26 @@ interface SkillChartProps {
 function SkillChart(props: SkillChartProps): JSX.Element {
   const { playerName, games } = props;
   let skill = 0;
-  const data = games.map((game) => {
+  const skillLine = games.map((game) => {
     skill += game.red.name == playerName ? game.red.skillChange : game.blue.skillChange;
     return {x: game.date * 1000, y: skill};
   });
+  const data = {datasets: [{
+    data: skillLine,
+    fill: false,
+    borderColor: '#0000FF',
+  }]};
+  const options = {
+    scales: {xAxes: [{type: 'time'}]},
+    legend: {display: false},
+    tooltips: {callbacks: {
+      title: (tooltip: any, point: any) => formatEpoch(tooltip[0].xLabel / 1000),
+      label: (tooltip: any, point: any) => tooltip.yLabel.toFixed(3),
+    }}
+  };
   return (
     <Panel header={'Skill Chart'}>
-      {games.length >= 2 &&
-        <VictoryChart>
-          <VictoryAxis scale={'time'}/>
-          <VictoryAxis dependentAxis label={'Skill'}/>
-          <VictoryLine data={data} style={{data: {stroke: '#0000FF'}}}/>
-        </VictoryChart>
-      }
+      <Line data={data} options={options}/>
     </Panel>
   );
 }
