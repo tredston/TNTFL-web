6030e2fdf8f624d30720f00a67627d7e4d3468a4
Thomas Redston <tlr@corefiling.com>
2016-11-09 16:43:12 +0000

    Share ladder charts

---
+++ ui/components/goals-chart.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Pie } from 'react-chartjs-2';
+
+import ChartHolder from './chart-holder';
+import { Totals } from '../model/player';
+
+export default function GoalsChart(totals: Totals): JSX.Element {
+  const data = {
+    labels: ['Against', 'For'],
+    datasets: [{
+      data: [totals.against, totals.for],
+      backgroundColor: ['#FF0000', '#0000FF'],
+    }]
+  }
+  const options = {
+    legend: {display: false},
+  }
+  return (
+    <ChartHolder>
+      <Pie data={data} options={options} />
+    </ChartHolder>
+  );
+}
