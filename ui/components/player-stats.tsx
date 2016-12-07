77e1511959be88a4647209181e5db12c4cac4d4b
Thomas Redston <tlr@corefiling.com>
2016-12-07 17:48:46 +0000

    Naming things

--- ui/components/player-stats.tsx
+++ ui/components/player-stats.tsx
@@ -8,7 +8,7 @@ import Player from '../model/player';
 import * as Palette from '../palette';
 import { getLadderLeagueClass, formatRankChange, getNearlyInactiveClass } from '../utils/utils';
 
-const global: CSSProperties = {
+const statStyle: CSSProperties = {
   fontSize: 'x-large',
   fontWeight: 'bold',
   textAlign: 'center',
@@ -22,7 +22,7 @@ interface StatBoxProps {
 }
 function StatBox(props: StatBoxProps): JSX.Element {
   const { title, children, style, classes } = props;
-  let mergedStyle = style ? Object.assign({}, global, style) : global;
+  const mergedStyle = style ? Object.assign({}, statStyle, style) : statStyle;
   return (
     <Panel header={<h3>{title}</h3>} style={mergedStyle} className={classes}>
       {children}
@@ -40,7 +40,7 @@ function InstantStatBox(props: InstantStatBoxProps): JSX.Element {
   const { title, at, children, base } = props;
   return (
     <Panel header={<h3>{title}</h3>}>
-      <div style={global}>{children}</div>
+      <div style={statStyle}>{children}</div>
       <div style={{textAlign: 'right'}}>
         {at ? <span>at <GameTime date={at} base={base} /></span> : <span>before first game</span>}
       </div>
@@ -59,7 +59,7 @@ function DurationStatBox(props: DurationStatBoxProps): JSX.Element {
   const { title, from, to, children, base } = props;
   return (
     <Panel header={<h3>{title}</h3>}>
-      <div style={global}>{children}</div>
+      <div style={statStyle}>{children}</div>
       {from && <div style={{textAlign: 'right'}}>From <GameTime date={from} base={base} /></div>}
       {to && <div style={{textAlign: 'right'}}>to <GameTime date={to} base={base} /></div>}
     </Panel>
