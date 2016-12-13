2754001e2ba4e00ae2625f9589f10490683c1324
Thomas Redston <tlr@corefiling.com>
2016-12-13 17:13:14 +0000

    Replace game pie chart with custom bar chart

--- ui/components/games-chart.tsx
+++ ui/components/games-chart.tsx
@@ -5,15 +5,14 @@ import { TablePieChart } from './table-charts';
 import { Totals } from '../model/player';
 
 export default function GamesChart(totals: Totals): JSX.Element {
-  const draws = totals.games - totals.wins - totals.losses;
-  const data = {
-    labels: ['Losses', 'Draws', 'Wins'],
-    datasets: [{
-      data: [totals.losses, draws, totals.wins],
-      backgroundColor: ['#FF0000', '#FFC200', '#0000FF'],
-    }]
-  }
+  const win = (totals.wins / totals.games) * 100;
+  const draw = ((totals.games - totals.wins - totals.losses) / totals.games) * 100;
+  const loss = (totals.losses / totals.games) * 100;
   return (
-    <TablePieChart data={data}/>
+    <div style={{display: 'flex', border: '1px solid black', width: '100%', height: 25}}>
+      <div style={{display: 'inline-block', height: '100%', backgroundColor: '#0000FF', width: `${win}%`}}/>
+      <div style={{display: 'inline-block', height: '100%', backgroundColor: '#FFC200', width: `${draw}%`}}/>
+      <div style={{display: 'inline-block', height: '100%', backgroundColor: '#FF0000', width: `${loss}%`}}/>
+    </div>
   );
 }
