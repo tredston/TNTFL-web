2755c47cc7c9a0f69efce32ab97c56f439ca34ce
Thomas Redston <thomas_redston@outlook.com>
2017-08-25 10:55:54 +0100

    Add storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        enforce: 'pre',
+        loader: 'tslint-loader',
+        exclude: [
+          'node_modules',
+          'swagger',
+        ],
+        options: {
+          emitErrors: true,
+        },
+      },
+      {
+        test: /\.ts(x?)$/,
+        loader: 'awesome-typescript-loader',
+        exclude: [
+          'node_modules',
+          'swagger',
+        ],
+        options: {
+          useCache: true,
+          useBabel: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devtool: 'inline-source-map',
+};
