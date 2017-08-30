0e3f568fa4cd9a491540dee17e1d360670b28dfb
Thomas Redston <thomas_redston@outlook.com>
2017-08-30 16:56:43 +0100

    Fix rank component's style

--- ui/components/player/rank-stat.tsx
+++ ui/components/player/rank-stat.tsx
@@ -16,9 +16,8 @@ export default function RankStatBox(props: RankStatBoxProps): JSX.Element {
   const league = getLadderLeagueClass(rank, numActivePlayers);
   const inactive = getNearlyInactiveClass(lastPlayed, now);
   const prettyRank = rank !== -1 ? rank : '-';
-  const rankStyle = Object.assign({}, style, {width: '100%'});
   return (
-    <StatBox title='Current Ranking' style={rankStyle} classes={`${league} ${inactive}`}>
+    <StatBox title='Rank' style={style} classes={`${league} ${inactive}`}>
       {prettyRank}
     </StatBox>
   );
